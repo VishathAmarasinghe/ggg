afc81cfc94ac9ebade9e9dc6e6336e909063dc4a
Rasanga Pramuddhika <91723368+pramuddhika@users.noreply.github.com>
2024-06-16 09:54:34 +0530

    dashboard updated

--- frontend/src/Components/SensordCards.jsx
+++ frontend/src/Components/SensordCards.jsx
@@ -1,43 +1,36 @@
-import SensorCard from "./SensorCard";
-import TemperatureVsTimeChart from "./TemperatureVsTimeChart";
+import React from 'react';
+import SensorCard from './SensorCard';
 
-const SensordCards = ({sensorData}) => {
-  console.log("incomming sensor dat ",sensorData);
+const SensordCards = ({ sensorData }) => {
+  console.log("incoming sensor data ", sensorData);
+
   return (
     <div className="w-full">
       <div className="w-full flex justify-center">
         <div className="flex box-context w-10/12 h-20 mt-4 rounded-lg border-2 border-green-700 bg-green-100">
-
           <div className="w-96 border-r-2 border-green-700">
-            <p className="text-2xl font-semibold pl-1">Average Temparature</p>
-            <p className="text-end italic font-bold p-1 text-4xl pr-3">{sensorData?.averageTemperature} °C</p>
+            <p className="text-2xl font-semibold pl-1">Average Temperature</p>
+            <p className="text-end italic font-bold p-1 text-4xl pr-3">{parseFloat(sensorData?.averageTemperature).toFixed(3)} °C</p>
           </div>
-
           <div className="w-96 border-r-2 border-green-700">
             <p className="text-2xl font-semibold pl-1">Cooling/Heating</p>
             <p className="text-end italic font-bold p-1 text-4xl pr-3">{sensorData?.coolTemp}</p>
           </div>
-
           <div className="w-96">
             <p className="text-2xl font-semibold pl-1">Fan Speed</p>
             <p className="text-end italic font-bold text-4xl pr-3">{sensorData?.fanSpeed} rpm</p>
           </div>
-
         </div>
       </div>
-
       <div className="mt-6">
-
         <div className="flex justify-around my-2">
-         <SensorCard sensorData={sensorData?.finalArray?.chartData} sensor={"1"}/>
-         <SensorCard sensorData={sensorData?.finalArray?.chartData} sensor={"2"}/>
+          <SensorCard sensorData={sensorData?.finalArray?.chartData} sensor={"1"} />
+          <SensorCard sensorData={sensorData?.finalArray?.chartData} sensor={"2"} />
         </div>
-
         <div className="flex justify-around">
-         <SensorCard sensorData={sensorData?.finalArray?.chartData} sensor={"3"}/>
-         <SensorCard sensorData={sensorData?.finalArray?.chartData} sensor={"4"}/>
+          <SensorCard sensorData={sensorData?.finalArray?.chartData} sensor={"3"} />
+          <SensorCard sensorData={sensorData?.finalArray?.chartData} sensor={"4"} />
         </div>
-        
       </div>
     </div>
   );
